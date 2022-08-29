9bf67375cee0f2131dbf81699855ad493085664d
Everaldo <torresjr1984@hotmail.com>
2022-08-29 12:56:31 -0300

    Adicionados novos estudos

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
     //
+    public function index(Request $r)
+    {
+        $addresses = Address::all();
+        return $addresses;
+    }
+
+    public function findOne(Request $r)
+    {
+        $addresses = Address::find($r->id);
+        return $addresses;
+    }
+
+    public function insert(Request $r)
+    {
+        $rawData = $r->only(['address']);
+
+        $address = Address::create($rawData);
+
+        return $address;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,23 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AddressController;
+
 use Illuminate\Support\Facades\Route;
 
 
 //Um usuário, inicialmente tem um endereço
 Route::get('/users', [UserController::class, 'index']);
+//Buscar um usuário pelo ID
+Route::get('/users/{id}', [UserController::class, 'findOne']);
+
+//Incluir um novo usuário
+Route::post('/users', [UserController::class, 'insert']);
 
 Route::get('/addresses', [AddressController::class, 'index']);
+
+// Buscar um endereço pelo ID
+Route::get('/addresses/{id}', [AddressController::class, 'findOne']);
+
+//Incluir um novo endereço
+Route::post('/addresses', [AddressController::class, 'insert']);
